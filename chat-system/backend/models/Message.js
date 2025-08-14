ccbfffffc8ee3747a01229050c71683be021e5b7
arpitat09 <arpita.genai@gmail.com>
2025-08-14 21:44:51 +0530

    Delete chat-system/backend directory

--- chat-system/backend/models/Message.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const messageSchema = new mongoose.Schema({
-  senderId: { type: String, required: true },
-  receiverId: { type: String, required: true },
-  content: { type: String, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Message', messageSchema);
